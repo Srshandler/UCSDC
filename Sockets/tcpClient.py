b24f80e6bdfb40f1a1693c15414fa3fbd003ae58
Srshandler <69633027+Srshandler@users.noreply.github.com>
2023-10-13 18:27:00 +0000

    Comit teste

--- Sockets/tcpClient.py
+++ Sockets/tcpClient.py
@@ -12,13 +12,13 @@ print(f"Servidor escutando em {host}:{port}")
 while True:
   # Aguarda por uma conexão
   client_socket, addr = server_socket.accept()
-    print(f"Conexão recebida de {addr}")  while True:
+  print(f"Conexão recebida de {addr}")  while True:
     # Recebe a mensagem do cliente    message = client_socket.recv(1024)
     # Se a mens estiver vazia, encerra a conexão 
   if not message:      break
-    print(f"Cliente diz: {message.decode()}")
+  print(f"Cliente diz: {message.decode()}")
 # Envia uma resposta para o cliente
-    response = "Mensagem recebida com sucesso!"  
-    client_socket.send(response.encode())
+  response = "Mensagem recebida com sucesso!"  
+  client_socket.send(response.encode())
 # Fecha a conexão com o cliente 
-    client_socket.close()
+  client_socket.close()
